6b260a41095f56b609f0aa245f0eaa96f6594361
ayguid <guidoaimar@gmail.com>
2021-08-14 22:51:56 -0300

    2nd commit

---
+++ dist/precache-manifest.ef16a032da98fb1e86aade3c6fdf37af.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a5ee45ba046e636727",
+    "url": "/feel-feed/css/about.9f80edd5.css"
+  },
+  {
+    "revision": "d38239f149dfddadd1ee",
+    "url": "/feel-feed/css/app.4b127eb2.css"
+  },
+  {
+    "revision": "eb6e708880e4ee37631d",
+    "url": "/feel-feed/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "a094467603e48507a8205939e0894b16",
+    "url": "/feel-feed/index.html"
+  },
+  {
+    "revision": "79a5ee45ba046e636727",
+    "url": "/feel-feed/js/about.d4e89e9f.js"
+  },
+  {
+    "revision": "d38239f149dfddadd1ee",
+    "url": "/feel-feed/js/app.1977b1f7.js"
+  },
+  {
+    "revision": "eb6e708880e4ee37631d",
+    "url": "/feel-feed/js/chunk-vendors.02b713c5.js"
+  },
+  {
+    "revision": "fecf5d8fc2b0f4673e955c817fa8ded8",
+    "url": "/feel-feed/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/feel-feed/robots.txt"
+  }
+]);
